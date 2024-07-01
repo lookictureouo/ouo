34a6ab0e39ee0db26b74331dc9c3215ad7b7ac08
Lookictureouo <zj22078@my.westlake.school.nz>
2024-07-01 12:50:38 +1200

    ouo

---
+++ Python.py
@@ -0,0 +1,15 @@
+# lst = input("choose a word: ")
+# for i in lst:
+#     print(i)
+
+# lst = [1,2,3,4,5]
+# for i in lst:
+#     print(i*i)
+
+# lst = [1,2,3,4,5,6,7,8,9,10]
+# lst.reverse()
+# for i in lst:
+#     print(i)
+
+lst = ["Alice", "Bob", "Charlie"]
+
