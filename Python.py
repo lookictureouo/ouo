683a5cef1523f956d5d6a3340781f46f00389beb
Lookictureouo <zj22078@my.westlake.school.nz>
2024-07-02 12:00:13 +1200

    Q11

--- Python.py
+++ Python.py
@@ -11,5 +11,25 @@
 # for i in lst:
 #     print(i)
 
-lst = ["Alice", "Bob", "Charlie"]
+# lst = ["Alice", "Bob", "Charlie"]
+# for i in lst:
+#     print("Hello, " + i)
 
+# Question 10:
+# number = range(1,11)
+# total = 0  # Initialize a variable to store the sum
+# for i in number:
+#     total += i
+# print(total)
+# OR
+# x = 0
+# for i in range(1,11):
+#     x = x + i
+# print(x)
+
+# Question 11:
+# lst = [10, 20, 30, 40, 50]
+# for i in lst:
+#     if i > 25:
+#         print(i)
+    
