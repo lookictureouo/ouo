86ca5e79ae6999a46ff60e19c791f0bf4d3b63f8
Lookictureouo <zj22078@my.westlake.school.nz>
2024-07-25 10:45:44 +1200

    aw

--- Pizza.py
+++ Pizza.py
@@ -1,7 +1,7 @@
 def menu():
     #You should display a menu here! Make it look pretty!
     print("What you want in your pizza?")
-    order()
+    order(the_menu, orderlist,the_menu_order)
     
 
 
@@ -11,15 +11,15 @@ def order(the_menu, orderlist,the_menu_order):
     for i in the_menu:
         print(i)
     while finish_order != True:
-        x = input("0.Done or Insert the ID of the food to order: " )
+        x = int(input("0.Done or Insert the ID of the food to order: " ))
         if x <= 12 and x > 0:
-            orderlist.append(the_menu_order(x-1))
+            orderlist.append(the_menu_order[x-1])
         elif x == 0:
             finish_order = True
-            total()
+            total(orderlist)
         else:
             print("An error has occurred, check what have you typed or context us by xxx-xxx-xxxx")
-    return order()
+            return order()
 
 def listchecking():
     user_finish_checking = False
@@ -33,7 +33,7 @@ def listchecking():
         x = input("0.Done @.Back or type the number of the item you want to remove: ")
         if x == 0:
             user_finish_checking = True
-            total()
+            total(orderlist)
         elif x == "@":
             order()
         else:
@@ -44,33 +44,35 @@ def total(orderlist):
     
         cost = 0
         for i in orderlist:
-            if i == the_menu_order(0): 
+            if i == the_menu_order[0]: 
                 cost += 2
-            elif i == the_menu_order(1):
+            elif i == the_menu_order[1]:
                 cost = cost + 1
-            elif i == the_menu_order(2):
+            elif i == the_menu_order[2]:
                 cost = cost + 5
-            elif i == the_menu_order(3):
+            elif i == the_menu_order[3]:
                 cost = cost + 3
-            elif i == the_menu_order(4):
+            elif i == the_menu_order[4]:
                 cost = cost + 2
-            elif i == the_menu_order(5):
+            elif i == the_menu_order[5]:
                 cost = cost + 3
-            elif i == the_menu_order(6):
+            elif i == the_menu_order[6]:
                 cost = cost + 3
-            elif i == the_menu_order(7):
+            elif i == the_menu_order[7]:
                 cost = cost + 4
-            elif i == the_menu_order(8):
+            elif i == the_menu_order[8]:
                 cost = cost + 4
-            elif i == the_menu_order(9):
+            elif i == the_menu_order[9]:
                 cost = cost + 6
-            elif i == the_menu_order(10):
+            elif i == the_menu_order[10]:
                 cost = cost + 3
-            elif i == the_menu_order(11):
+            elif i == the_menu_order[11]:
                 cost = cost + 1
-        print(cost) 
+        print(cost, end=" ") 
+        print("$")
 
 orderlist = []
+food_menu = ["self order pizza", ""]
 the_menu = ["1. Cheese","2. Tomato Sauce","3. Pineapple","4. Olive","5. Onion","6. Broccoli","7. Artichokes","8. Cauliflower","9. Pepperoni","10. Sausage","11. Bacon","12. Mushroom"]
 the_menu_order = ["Cheese","Tomato Sauce","Pineapple","Olive","Onion","Broccoli","Artichokes","Cauliflower","Pepperoni","Sausage","Bacon","Mushroom"]
 user_finish_checking = False
