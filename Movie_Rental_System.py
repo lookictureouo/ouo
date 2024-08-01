c1b9e10b31a175065afac64cd75ca3e26ab6595b
Lookictureouo <zj22078@my.westlake.school.nz>
2024-08-02 10:15:12 +1200

    Bug for rental report

--- Movie_Rental_System.py
+++ Movie_Rental_System.py
@@ -1,6 +1,3 @@
-def menu():
-    pass
-
 def search():
     pass
 
@@ -21,184 +18,56 @@ def rent(movie_prize, user_order, movie_list):
 def rent2(movie_prize, user_order, movie_list, inventory):
     finish_renting = False
     user = False
+    found = False
     print("Do you want to rent 0.rental_report 1.UP 2.The Hunger Game 3.Hachi: A Dog's Tale 4.Alex's Autobiography 5.FATE 6.Back to menu 7.Exit")
     user_input = int(input(": "))
     try:
         print(user_input)
     except ValueError:
         print("Please only insert 0, 1, 2, 3, 4, 5, 6, 7") #checking user only insert 1,2 3,4,5,6,7
-    if user_input != 1 and user_input != 2 and user_input != 3 and user_input != 4 and user_input != 5 and user_input != 6 and user_input != 7:
+    if user_input != 0 and user_input != 1 and user_input != 2 and user_input != 3 and user_input != 4 and user_input != 5 and user_input != 6 and user_input != 7:
         print("Please only insert 0, 1, 2, 3, 4, 5, 6, 7") #checking user only insert 1,2 3,4,5,6,7
         return rent2(movie_prize, user_order, movie_list, inventory)
     else:
         while finish_renting !=  True:
             if user_input == 0: #Report
-                rental_report()
-            elif user_input == 1: #For UP
+                rental_report(user_order)
+            elif user_input in [1, 2, 3, 4, 5]:
                 for stuff in inventory:
-                    if stuff == "1":
-                        user_order.append("1") #Add UP in User inventory
-                        inventory.remove("1") #Remove UP in inventory
+                    if stuff == str(user_input):
+                        user_order.append(stuff)  # Add movie to User inventory
+                        inventory.remove(stuff)   # Remove movie from inventory
                         finish_renting = True
+                        found = True
                         return rent2(movie_prize, user_order, movie_list, inventory)
-                    else:
-                        pass
-
-                    while user != True:
-                                print("UP is out of stock, 1.rent other movie 2.menu 3.rental_report 4.Exit")
-                                user_input = int(input(": "))
-                                try:
-                                    print(user_input)
-                                except ValueError:
-                                    print("Please only insert 1, 2, 3, 4") #checking user only insert 1,2 3,4
-                                    return rent2(movie_prize, user_order, movie_list, inventory)
-                                if user_input != 1 and user_input != 2 and user_input != 3 and user_input != 4:
-                                    print("Please only insert 1, 2, 3, 4") #checking user only insert 1,2 3,4
-                                elif user_input == 1:
-                                    user = True
-                                    return rent2(movie_prize, user_order, movie_list, inventory)
-                                elif user_input == 2:
-                                    user = True
-                                    return menu()
-                                elif user_input == 3:
-                                    user = rental_report()
-                                elif user_input == 4:
-                                    user = True
-                                    print("Thanks for stopping by!\n")
-                                else:
-                                        print("Unknown Error Occur") #For testing
-            elif user_input == 2: #For Hunger Game
-                for stuff in inventory:
-                    if stuff == "2":
-                        user_order.append("2") #Add Hunger Game in User inventory
-                        inventory.remove("2") #Remove Hunger Game in inventory
-                        finish_renting = True
+                                      
+                if not found:
+                    print(f"Movie {user_input} is out of stock, 1.rent other movie 2.menu 3.rental_report 4.Exit")
+                    user_input = int(input(": "))
+                    try:
+                        print(user_input)
+                    except ValueError:
+                        print("Please only insert 1, 2, 3, 4") #checking user only insert 1,2 3,4
                         return rent2(movie_prize, user_order, movie_list, inventory)
-                    else:
-                        pass
-
                     while user != True:
-                                print("The Hunger Game is out of stock, 1.rent other movie 2.menu 3.rental_report 4.Exit")
-                                user_input = int(input(": "))
-                                try:
-                                    print(user_input)
-                                except ValueError:
-                                    print("Please only insert 1, 2, 3, 4") #checking user only insert 1,2 3,4
-                                    return rent2(movie_prize, user_order, movie_list, inventory)
-                                if user_input != 1 and user_input != 2 and user_input != 3 and user_input != 4:
-                                    print("Please only insert 1, 2, 3, 4") #checking user only insert 1,2 3,4
-                                elif user_input == 1:
-                                    user = True
-                                    return rent2(movie_prize, user_order, movie_list, inventory)
-                                elif user_input == 2:
-                                    user = True
-                                    return menu()
-                                elif user_input == 3:
-                                    user = rental_report()
-                                elif user_input == 4:
-                                    user = True
-                                    print("Thanks for stopping by!\n")
-                                else:
-                                        print("Unknown Error Occur") #For testing
-            elif user_input == 3: #For Hachi: A Dog's Tale
-                for stuff in inventory:
-                    if stuff == "3":
-                        user_order.append("3") #Add Hachi: A Dog's Tale in User inventory
-                        inventory.remove("3") #Remove Hachi: A Dog's Tale in inventory
-                        finish_renting = True
-                        return rent2(movie_prize, user_order, movie_list, inventory)
-                    else:
-                        pass
+                        if user_input != 1 and user_input != 2 and user_input != 3 and user_input != 4:
+                            print("Please only insert 1, 2, 3, 4") #checking user only insert 1,2 3,4                
+                        elif user_input == 1:
+                            user = True
+                            return rent2(movie_prize, user_order, movie_list, inventory)
+                        elif user_input == 2:
+                            user = True
+                            return menu()
+                        elif user_input == 3:
+                            user = True
+                            return rental_report(user_order)
+                        elif user_input == 4:
+                            user = True
+                            print("Thanks for stopping by!!!!!!!!!!!!\n")
+                        else:
+                            print("Unknown Error Occur") #For testing
 
-                    while user != True:
-                                print("Hachi: A Dog's Tale is out of stock, 1.rent other movie 2.menu 3.rental_report 4.Exit")
-                                user_input = int(input(": "))
-                                try:
-                                    print(user_input)
-                                except ValueError:
-                                    print("Please only insert 1, 2, 3, 4") #checking user only insert 1,2 3,4
-                                    return rent2(movie_prize, user_order, movie_list, inventory)
-                                if user_input != 1 and user_input != 2 and user_input != 3 and user_input != 4:
-                                    print("Please only insert 1, 2, 3, 4") #checking user only insert 1,2 3,4
-                                elif user_input == 1:
-                                    user = True
-                                    return rent2(movie_prize, user_order, movie_list, inventory)
-                                elif user_input == 2:
-                                    user = True
-                                    return menu()
-                                elif user_input == 3:
-                                    user = rental_report()
-                                elif user_input == 4:
-                                    user = True
-                                    print("Thanks for stopping by!\n")
-                                else:
-                                        print("Unknown Error Occur") #For testing
-            elif user_input == 4: #For Alex's Autobiography
-                for stuff in inventory:
-                    if stuff == "4":
-                        user_order.append("4") #Add Alex's Autobiography in User inventory
-                        inventory.remove("4") #Remove Alex's Autobiography in inventory
-                        finish_renting = True
-                        return rent2(movie_prize, user_order, movie_list, inventory)
-                    else:
-                        pass
-
-                    while user != True:
-                                print("Alex's Autobiography is out of stock, 1.rent other movie 2.menu 3.rental_report 4.Exit")
-                                user_input = int(input(": "))
-                                try:
-                                    print(user_input)
-                                except ValueError:
-                                    print("Please only insert 1, 2, 3, 4") #checking user only insert 1,2 3,4
-                                    return rent2(movie_prize, user_order, movie_list, inventory)
-                                if user_input != 1 and user_input != 2 and user_input != 3 and user_input != 4:
-                                    print("Please only insert 1, 2, 3, 4") #checking user only insert 1,2 3,4
-                                elif user_input == 1:
-                                    user = True
-                                    return rent2(movie_prize, user_order, movie_list, inventory)
-                                elif user_input == 2:
-                                    user = True
-                                    return menu()
-                                elif user_input == 3:
-                                    user = rental_report()
-                                elif user_input == 4:
-                                    user = True
-                                    print("Thanks for stopping by!\n")
-                                else:
-                                        print("Unknown Error Occur") #For testing
-            elif user_input == 5: #For FATE
-                for stuff in inventory:
-                    if stuff == "5":
-                        user_order.append("5") #Add FATE in User inventory
-                        inventory.remove("5") #Remove FATE in inventory
-                        finish_renting = True
-                        return rent2(movie_prize, user_order, movie_list, inventory)
-                    else:
-                        pass
-
-                    while user != True:
-                                print("FATE is out of stock, 1.rent other movie 2.menu 3.rental_report 4.Exit")
-                                user_input = int(input(": "))
-                                try:
-                                    print(user_input)
-                                except ValueError:
-                                    print("Please only insert 1, 2, 3, 4") #checking user only insert 1,2 3,4
-                                    return rent2(movie_prize, user_order, movie_list, inventory)
-                                if user_input != 1 and user_input != 2 and user_input != 3 and user_input != 4:
-                                    print("Please only insert 1, 2, 3, 4") #checking user only insert 1,2 3,4
-                                elif user_input == 1:
-                                    user = True
-                                    return rent2(movie_prize, user_order, movie_list, inventory)
-                                elif user_input == 2:
-                                    user = True
-                                    return menu()
-                                elif user_input == 3:
-                                    user = rental_report()
-                                elif user_input == 4:
-                                    user = True
-                                    print("Thanks for stopping by!\n")
-                                else:
-                                        print("Unknown Error Occur") #For testing
+            
             elif user_input == 6: #Menu
                 menu()
             elif user_input == 7: #EXIT
@@ -231,8 +100,9 @@ def menu():
 def returns():
     pass
 
-def rental_report():
-    pass
+def rental_report(user_order):
+    for i in user_order:
+        print(i)
 
 user_exit = False
 inventory = ["1","2","3","4","5"]
