4974b07d8a6c657d9c7347c5b498552668bc445f
Lookictureouo <zj22078@my.westlake.school.nz>
2024-08-06 15:10:39 +1200

    ser_guess == answer bug int

--- Number_Guessing.py
+++ Number_Guessing.py
@@ -9,26 +9,46 @@ def game():
         user_guess = input(f"{user_name}" + ", " + "Please Take your guess: ")
         try_left -= 1
         if user_guess == answer:
-            pass
+            try_left = 0
+            print("congratulations!!!")
+            again()
         else:
-            pass
+            print("You are so close, please try again.")
+        
+        if try_left == 0:
+            print("Sorry " f"{user_name}, you have uses all your trys")
+            again()
 
+def again():
+    print("play again? Y or N") 
+    playagain = input(":" )
+    if playagain.lower() == "y":
+        game()
+    elif playagain.lower() == "n":
+        print("Thank u for playing")
+    else: 
+        print("Please only insert Y or N")
+        return again()
+
+def to_start():
+    start = input("Press 1 to stat the Game: ")
+    try:
+        print(start)
+    except ValueError:
+        print("Please only insert 1") #checking user only insert 1
+    if start == "1":
+        game()
+    else:
+        print("Please only insert 1")
+        return to_start()
 
 
 print("=========================")
 print("  Number Guessing Game")
 print("=========================")
 user_name = input("Whats your name: ")
-print("Hi, " + f"{user_name} ", + "Welcome to the Number Guessing Game")
+print("Hi, " + user_name + ", Welcome to the Number Guessing Game")
 print("You will being asked to guess a number from 1 to 5.")
 print("You have 3 chance to gets the correct number.")
-start = input("Press 1 to stat the Game: ")
-try:
-    print(start)
-except ValueError:
-    print("Please only insert 1") #checking user only insert 1
-if start == 0:
-    game()
-else:
-    print("Please only insert 1")
+to_start()
 
